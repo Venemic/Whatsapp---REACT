ac5bf5e3a4740313accba83190da62745bcbac86
Venemic <shivamsingh6318@gmail.com>
2021-05-09 17:36:03 +0530

    second

--- pages/Loading.js
+++ pages/Loading.js
@@ -1,17 +1,26 @@
 import {Circle} from "better-react-spinkit";
-import firebase from "firebase"
-
+import styled from 'styled-components';
 function Loading() {
     return (
-        <center style={{display:"grid",placeholder: "center",height:"100vh"}}>
+        <Center >
              <div>
                     <img alt="" height={200} style={{marginBottom:10}} src="https://upload.wikimedia.org/wikipedia/commons/thumb/6/6b/WhatsApp.svg/897px-WhatsApp.svg.png"/>
+                    
             </div>
-            <Circle color="#4BC55A" size={60} />
-        </center>
+           <Circle color="#4BC55A" size={60} />
+        </Center>
        
     )
 }
 
 export default Loading
 
+
+const Center = styled.section`
+    overflow: hidden;
+    display:flex;
+    flex-direction: column;
+    justify-content: center;
+     align-items: center;
+     margin-top:250px;
+`;
